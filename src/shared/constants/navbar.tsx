810e9ac8c3084ca203ca5e1528939b6e8d9906b5
GustavoFLuz <gu.luz400@gmail.com>
2023-05-05 16:24:32 -0300

    Hidden projects by now

--- src/shared/constants/navbar.tsx
+++ src/shared/constants/navbar.tsx
@@ -5,7 +5,7 @@ import EmailIcon from '@mui/icons-material/Email';
 export const navbarConstants = {
     sections: [
         { title: { en: 'About me', pt: 'Sobre mim' }, id: '#' },
-        { title: { en: 'Projects', pt: 'Projetos' }, id: '#' },
+        /* { title: { en: 'Projects', pt: 'Projetos' }, id: '#' }, */
         { title: { en: 'Skills', pt: 'Habilidades' }, id: '#' },
         { title: { en: 'Contact', pt: 'Contato' }, id: '#' },
     ],
